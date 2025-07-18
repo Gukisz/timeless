4bb3bebc9429e11a6e7a58f931d38cda5569ff7d
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2025-07-18 20:40:13 -0300

    feat: create createRestRoutes

---
+++ backend/src/utils/createRestRoutes.ts
@@ -0,0 +1,21 @@
+import { Router, RequestHandler } from "express";
+
+interface RestController {
+    list: RequestHandler;
+    create: RequestHandler;
+    show: RequestHandler;
+    update: RequestHandler;
+    delete: RequestHandler;
+}
+
+export function createRestRoutes(basePath: string, controller: RestController): Router {
+    const router = Router();
+
+    router.get(`/${basePath}`, controller.list);
+    router.post(`/${basePath}`, controller.create);
+    router.get(`/${basePath}/:id`, controller.show);
+    router.patch(`/${basePath}/:id`, controller.update);
+    router.delete(`/${basePath}/:id`, controller.delete);
+
+return router;
+}
